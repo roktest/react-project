e1c0d78f68eb875d82974e53cc0e5efcb1e681ef
Rodrigo Katz <rodrigokatztest@gmail.com>
2023-03-23 09:37:56 -0300

    listening-events-and-event-handlers: added onClick event handler

--- src/components/expensesUI/ExpenseItem/ExpenseItem.js
+++ src/components/expensesUI/ExpenseItem/ExpenseItem.js
@@ -4,13 +4,22 @@ import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../generalUI/Card/Card";
 
 const ExpenseItem = (expenseItemProps) => {
+  let title = expenseItemProps.title;
+
+  const clickHandler = () => {
+    console.log('clicked');
+    title = 'Updated';
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenseItemProps.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2 className="expense-item__title">{expenseItemProps.title}</h2>
+        <h2 className="expense-item__title">{title}</h2>
         <div className="expense-item__price">$ {expenseItemProps.price}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
