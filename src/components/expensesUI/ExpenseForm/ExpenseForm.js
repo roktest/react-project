e841a5adf155561e9806a75e190ce2ffae14045f
Rodrigo Katz <rodrigokatztest@gmail.com>
2023-03-27 16:22:49 -0300

    two-way-binding: added 2way binding to be able to listen and set form values

--- src/components/expensesUI/ExpenseForm/ExpenseForm.js
+++ src/components/expensesUI/ExpenseForm/ExpenseForm.js
@@ -1,58 +1,76 @@
 import React, { useState } from "react";
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
-const ExpenseForm = ()=>{
+const ExpenseForm = () => {
+  const [currentTitle, setCurrentTitle] = useState("");
+  const [currentAmount, setCurrentAmount] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
 
-    const [currentTitle, setCurrentTitle] = useState('');
-    const [currentAmount, setCurrentAmount] = useState('');
-    const [currentDate, setCurrentDate] = useState('');
+  const titleOnChangeHandler = (event) => {
+    //console.log(event);
+    //console.log(event.target.value);
+    setCurrentTitle(event.target.value);
+  };
 
-    const titleOnChangeHandler = (event) => {
-        //console.log(event);
-        //console.log(event.target.value);
-        setCurrentTitle(event.target.value);
-    }
+  const amountOnChangeHandler = (event) => {
+    setCurrentAmount(event.target.value);
+  };
 
-    const amountOnChangeHandler = (event)=>{
-        setCurrentAmount(event.target.value);
+  const dateOnChangeHandler = (event) => {
+    setCurrentDate(event.target.value);
+  };
+
+  const formSubmition = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: currentTitle,
+      amount: currentAmount,
+      date: new Date(currentDate),
     };
 
-    const dateOnChangeHandler = (event)=>{
-        setCurrentDate(event.target.value);
-    };
+    console.log(expenseData);
+    setCurrentTitle("");
+    setCurrentAmount("");
+    setCurrentDate("");
+  };
 
-    const formSubmition = (event) => {
-        event.preventDefault();
-        const expenseData = {
-            title: currentTitle,
-            amount: currentAmount,
-            date: new Date(currentDate)
-        }
-
-        console.log(expenseData);
-    };
-
-    return(
-            <form onSubmit={formSubmition}>
-                <div className="new-expense__controls">
-                    <div className="new-expense__control">
-                        <label>Title</label>
-                        <input type="text" onChange={titleOnChangeHandler}/>
-                    </div>
-                    <div className="new-expense__control">
-                        <label>Amount</label>
-                        <input type="text" min="0.01"   step="0.01" onChange={amountOnChangeHandler}/>
-                    </div>
-                    <div className="new-expense__control">
-                        <label>Date</label>
-                        <input type="date" min="01-01-2019" max="01-01-2050" onChange={dateOnChangeHandler}/>
-                    </div>
-                </div>
-                <div className="new-expense__actions">
-                    <button type="submit">Add expense</button>
-                </div>
-            </form>
-    );
+  return (
+    <form onSubmit={formSubmition}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={currentTitle}
+            onChange={titleOnChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="text"
+            min="0.01"
+            step="0.01"
+            value={currentAmount}
+            onChange={amountOnChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="01-01-2019"
+            max="01-01-2050"
+            value={currentDate}
+            onChange={dateOnChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
